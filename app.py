f0aafbacd0dddb80d5d4ed2493bd9962652f0267
Travis Tidwell <ttidwell0117@outlook.com>
2025-01-02 15:26:21 -0600

    added more data for rendering

--- app.py
+++ app.py
@@ -2,8 +2,19 @@ import streamlit as st
 import pandas as pd
 from plotly import express as px
 
-st.write("Hello, world!")
 
 df = pd.read_csv("vehicles_us.csv")
 
 st.dataframe(df.head(10))
+
+st.write("### Price Distribution")
+fig = px.histogram(df, x='price', title='Distribution of Prices')
+st.plotly_chart(fig)
+
+st.write("### Price vs Year")
+scatter = px.scatter(df, x='year', y='price', title='Price vs Year')
+st.plotly_chart(scatter)
+
+if st.checkbox("Show Raw Data"):
+    st.write(df)
+    
